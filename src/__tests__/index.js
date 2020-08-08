99c2bf956afadb804fca30417290f7f60d7d882d
Me <matteo@Mes-MacBook-Pro.local>
2020-08-08 15:46:58 +1000

    Refactored and added global hook to mimic devtools

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,57 +1,38 @@
 const puppeteer = require('puppeteer');
-const reactPinpoint = require('react-pinpoint');
 const path = require('path');
-require('./cycle.js');
-console.log(JSON.decycle);
+
 (async () => {
-    const pathToExtension = '~/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.8.2_0';
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: [
-        `--disable-extensions-except=${pathToExtension}`,
-        `--load-extension=${pathToExtension}`
-        ]
-    });
-    // const mattIsAFlag = true;
-    // const targets = await browser.targets();
-    // const backgroundPageTarget = targets.find(target => target.type() === 'background_page');
-    // const backgroundPage = await backgroundPageTarget.page();
-    // const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
+
+    // Set an empty object on devtools hook so react will record fibers
+    // Must exist before react runs
+    await page.evaluateOnNewDocument(() => window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = {})
+
     await page.goto('http://localhost:3000/');
-
-    // // exposing react-pinpoint function to puppeteer browser context
-    // await page.exposeFunction('random', () => reactPinpoint.random());
-
-    // // calling the exposed function in puppeteer browser
-    // await page.evaluate(() => {
-    //     console.log('random', random().then(result=>console.log(result)))
-    // })
 
     // testing adding react-pinpoint via a script tag
     await page.addScriptTag({
         path: path.join(__dirname, 'utils.js')
     })
-    await page.addScriptTag({
-        path: path.join(__dirname, 'cycle.js')
-    })
+
     await page.evaluate(() => {
         const root = document.querySelector('#root');
         mountToReactRoot(root);
     });
+
+
     await page.click('#yeah9')
     await page.click('#yeah9')
     await page.click('#yeah9')
-    const YEAH = await page.evaluate(async () => {
+
+    const slowRenders = await page.evaluate(async () => {
         console.log('changes->', changes);
         console.log('slow renders->', getAllSlowComponentRenders(0, changes))
-        const taie = getAllSlowComponentRenders(0, changes)
-        return await taie;
-        // return 'mattie is 5'
+        return getAllSlowComponentRenders(0, changes);
     });
 
-    console.log('YEAH->', YEAH);
-    // console.log('YEAH->', YEAH)
+    console.log('YEAH->', slowRenders);
     
 
     /* tracing using chrome dev tools
